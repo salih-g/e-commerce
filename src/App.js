eeed97166b389f3801938ad856f8dc2d1a82797c
Salih <sudanmerinosu@gmail.com>
2021-06-23 20:13:31 +0300

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import Products from './components/Products';
+import { Navbar, Products } from './components';
 
 const App = () => {
 	return (
 		<div>
+			<Navbar />
 			<Products />
 		</div>
 	);
